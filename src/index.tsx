9be6a3ffcab7b08f418149ce6118bf64f63debe6
danillyamkin <danilwebdev@gmail.com>
2022-10-14 00:54:46 +0300

    Доработан конфиг eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
 import { ChakraProvider } from '@chakra-ui/react'
+
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
