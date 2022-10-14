f32a094ac0703cf0836927548dbbe6dce9710212
danillyamkin <danilwebdev@gmail.com>
2022-10-14 19:52:56 +0300

    Chakra UI заменена на headless ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { ChakraProvider } from '@chakra-ui/react'
-
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
 )
