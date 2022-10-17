14f36aeda13a962f4d33a4894e954a59f371a7bb
danillyamkin <danilwebdev@gmail.com>
2022-10-17 03:48:05 +0300

    Изменена конфигурация роутера на работу с outlet

--- src/components/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,27 +1,19 @@
 import React, { FunctionComponent } from 'react'
+import { Outlet } from 'react-router-dom'
 
-import { DashboardProvider } from '../../../context/DashboardContext'
 import Sidebar from '../../widgets/Sidebar/Sidebar'
 
 import { navigationConfig } from './navigationConfig'
 
-interface DashboardLayoutProps {
-  children?: React.ReactNode
-}
-
-type Props = DashboardLayoutProps
-
-const DashboardLayout: FunctionComponent<Props> = ({
-  children,
-}): JSX.Element => {
+const DashboardLayout: FunctionComponent = (): JSX.Element => {
   return (
-    <DashboardProvider>
-      <div className={'flex h-screen w-full'}>
-        <Sidebar navigationConfig={navigationConfig} />
+    <div className={'flex h-screen w-full'}>
+      <Sidebar navigationConfig={navigationConfig} />
 
-        <main className={'h-screen flex-1'}>{children}</main>
-      </div>
-    </DashboardProvider>
+      <main className={'h-screen flex-1'}>
+        <Outlet />
+      </main>
+    </div>
   )
 }
 
